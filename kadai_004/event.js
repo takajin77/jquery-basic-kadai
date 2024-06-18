6d9d234e45bf80a71408325fc7149a4edeef76f6
takajin77 <takad7@gmail.com>
2024-06-18 14:29:13 +0900

    Add files via upload

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,6 +1,7 @@
-$(function () {
-  console.log('loadイベントが発生しました。')
-  $(document).on('scroll', () => {
-    console.log('scrollイベントが発生しました。');
-  });
+$(window).on ('load',()=> {
+  console.log('loadイベントが発生しました。');
+});
+
+$(document).on ('scroll', ()=> {
+  console.log('scrollイベントが発生しました。');
 });
